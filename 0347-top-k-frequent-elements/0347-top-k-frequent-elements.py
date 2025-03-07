8dd85bb2b57f1149a3db6d0eb13c517d7e67326f
Jenny <77416265+yjennykim@users.noreply.github.com>
2025-03-06 20:42:47 -0600

    Time: 7 ms (52.22%), Space: 21.4 MB (40.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,19 +6,7 @@ class Solution:
         for num in nums:
             counter[num] = counter.get(num, 0) + 1
         
-        # sort by value
-        counterAsList = []
-        for num, count in counter.items():
-            heapq.heappush(counterAsList, (count, num))
-
-        print(counterAsList)
-        largestKItems = heapq.nlargest(k, counterAsList)
-        
-        res = []
-        for count, num in largestKItems:
-            res.append(num)
-
-        return res
+        return [num for num, count in heapq.nlargest(k, counter.items(), key=lambda x: x[1])]
         
 
 
