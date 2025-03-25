d411c547b72d000e07b9598d70ec0e03763f435a
Jenny <77416265+yjennykim@users.noreply.github.com>
2025-03-24 20:06:30 -0700

    simplified solution: https://leetcode.com/problems/valid-palindrome-ii/description

--- 0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii.py
@@ -1,40 +1,22 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        # try deleting the left character first
-        l,r = 0, len(s)-1
-        deletedOnce = False
-        needsDeleteRight = False
-        while l <= r:
-            if s[l].lower() != s[r].lower():
-                if deletedOnce: # if already used deletion, break
-                    needsDeleteRight = True
-                    break
-                l += 1 # ignore left character
-                deletedOnce = True
-            else:
-                l += 1
-                r -= 1
 
-        # if deleting left character results in valid palindrome, return True
-        if not needsDeleteRight:
-            return True
-            
-        # try deleting the right character next
-        l,r = 0 , len(s)-1
-        deletedOnce = False
-        while l <= r:
-            if s[l].lower() != s[r].lower():
-                if deletedOnce: # if already used deletion, return False
+        def isValidPalindrome(l,r):
+            while l <= r:
+                if s[l] != s[r]:
                     return False
-                r -= 1
-                deletedOnce = True
-            else:
+
                 l += 1
                 r -= 1
+                
+            return True
+
+        l,r = 0, len(s)-1
+        while l <= r:
+            if s[l] != s[r]:
+                return isValidPalindrome(l+1, r) or isValidPalindrome(l, r-1)
             
+            l += 1
+            r -= 1
+        
         return True
-
-        
-        
-
-            
