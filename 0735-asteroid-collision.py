c626266a3019bc9cbaa2f0027717f17a889f0f0f
Jenny <77416265+yjennykim@users.noreply.github.com>
2025-04-27 18:37:59 -0500

    solved problem: https://leetcode.com/problems/asteroid-collision/description

--- 0735-asteroid-collision.py
+++ 0735-asteroid-collision.py
@@ -1,18 +1,23 @@
-from typing import List
-
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        stack = []
-        
+        """
+            If asteroid is negative, then it is going left so we should compare to elements in stack
+            
+        """
+        st = []  
         for asteroid in asteroids:
-            while stack and asteroid < 0 < stack[-1]:
-                if stack[-1] < -asteroid:
-                    stack.pop()  # current asteroid is larger; continue checking
-                    continue
-                elif stack[-1] == -asteroid:
-                    stack.pop()  # both explode
-                break
-            else:
-                stack.append(asteroid)
+            while st and st[-1] > 0 and asteroid < 0:
+                if abs(asteroid) > abs(st[-1]):
+                    st.pop()
+                elif abs(st[-1]) > abs(asteroid):
+                    asteroid = 0
+                else:
+                    # both get destroyed
+                    st.pop()
+                    asteroid = 0
+
+            if asteroid != 0:
+                st.append(asteroid)
         
-        return stack
+        return st
+        
