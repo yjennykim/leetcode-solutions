44293d2b2b31fcd9f80142102dd82f6cfbafe03e
Jenny <77416265+yjennykim@users.noreply.github.com>
2025-04-27 19:44:09 -0500

    solved problem: https://leetcode.com/problems/daily-temperatures/description

--- 0739-daily-temperatures.py
+++ 0739-daily-temperatures.py
@@ -1,17 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        Input: temperatures = [73,74,75,71,69,72,76,73]
-        days = [0] * len(temperatures)
-        st = []
+        days = [0] * len(temperatures) 
+        st = []  # previous days that need to be processed - monotonically decreasing stack
 
         for i,temp in enumerate(temperatures):
             while st and temp > st[-1][-1]:
                 j, small_temp = st.pop()
-                days[j] = i-j
+                days[j] = i-j  # update all previous days which are colder
 
             st.append((i,temp))
         
         return days
-            
-                
-            
