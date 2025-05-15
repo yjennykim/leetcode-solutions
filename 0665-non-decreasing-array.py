73bcc7d1a64c9d7d48d041275d0153bd07a8abfd
Jenny <77416265+yjennykim@users.noreply.github.com>
2025-05-14 22:32:52 -0700

    solved problem: https://leetcode.com/problems/non-decreasing-array/description

--- 0665-non-decreasing-array.py
+++ 0665-non-decreasing-array.py
@@ -1,22 +1,20 @@
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-        flag = 0
+        has_violation = False
         for i in range(1, len(nums)):
-            if nums[i-1] > nums[i]: # decreasing
-                if flag > 0:
+            if nums[i-1] > nums[i]:
+                if has_violation:
                     return False
 
-                if i == 1 or i == len(nums)-1: # simply update to really small number or really big number
-                    flag += 1
-                    continue
-                
-                print(f"nums[i-2]={nums[i-2]}, nums[r+1]={nums[i+1]}")
-                if not (nums[i-2] <= nums[i-1] <= nums[i+1]): # left is incorrect
-                    nums[i-1] = nums[i-2]
-                    flag += 1
-                
-                if not (nums[i-2] <= nums[i-1] <= nums[i] <= nums[i+1]): # right is incorrect
-                    nums[i] = nums[i+1]
-                    flag += 1      
+                has_violation = True
 
-        return flag <= 1
+                # Case 1: Nums[i-1] needs to be higher
+                if i == 1 or nums[i-2] <= nums[i]:
+                    nums[i-1] = nums[i]
+
+                # Case 2: Nums[i] needs to be lowere
+                else:
+                    nums[i] = nums[i-1]
+                
+        return True
+            
