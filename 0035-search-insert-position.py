225bdd4c6b8087921b445e7aa3563725f3c4b86c
Jenny <77416265+yjennykim@users.noreply.github.com>
2025-05-31 22:58:08 -0700

    solved problem: https://leetcode.com/problems/search-insert-positiondescription

--- 0035-search-insert-position.py
+++ 0035-search-insert-position.py
@@ -1,19 +1,11 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
-        
-        while l <= r:
+        l,r = 0, len(nums)
+        while l < r:
             m = (l + r) // 2
             if nums[m] < target:
                 l = m + 1
-            elif nums[m] > target:
-                r = m - 1
             else:
-                return m
+                r = m
         
-        return l
-
-        
-            
-            
-            
+        return r
